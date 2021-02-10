59136e9b7a1c83b225b964a2efe677cd8e304679
Chris McAnally <chris@adadevelopersacademy.org>
2021-02-10 10:37:00 -0800

    cloud functions index.js

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,22 @@
-const functions = require('firebase-functions')
+const functions = require('firebase-functions');
+const app = require('express')();
 
-module.exports = {
+const { loginUser, } = require('./APIs/users');
 
-  hello: functions.https.onRequest((req, res) => {
 
-    const subject = req.query.subject || 'World'
 
-    res.send(`Hello ${subject}!`)
+const {
+  getAllTodos,
+  postOneTodo,
+  deleteTodo,
+  updateTodo,
+} = require('./APIs/todos');
 
-  }),
-}
+app.get('/todos', getAllTodos);
+app.post('/todos', postOneTodo);
+app.delete('/todos/:todoId', deleteTodo);
+app.put('/todos/:todoId', updateTodo);
 
+app.post('/login', loginUser);
+
+exports.api = functions.https.onRequest(app);
